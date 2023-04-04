b10667c810551ceb7376c0d49f16ed6ac7a7b6e3
BrunonDEV <brunonblok@gmail.com>
2023-04-04 10:32:23 +0200

    fixed EDF
    fixed EDF defaulting to SSTF with changed SWITCH_TARGET_MID_FLY (this flag was affected by EDF, leading to different results of these two algorithms, when they should be the same)

--- disk_scheduling_src/classes/algorithms/EDF.ts
+++ disk_scheduling_src/classes/algorithms/EDF.ts
@@ -12,16 +12,12 @@ export default class EDF extends DeltaToTargetAlgorithm implements AccessAlgorit
     readOnFly: boolean = false
     realTime: boolean = true
 
+    /**
+     * switch target ONLY if previous target has been reached
+     */
     SWITCH_TARGET_MID_FLY: boolean = true
     
     getNextTarget(current_position: number, calls: ReadCall[], max_position?: number, current_time?: number, currentTarget?: ReadCall): ReadCall {
-        /**
-         * SWITCH_TARGET_MID_FLY
-         *  - switch target ONLY if previous target has been reached
-         */
-        if (currentTarget != null && !this.SWITCH_TARGET_MID_FLY)
-            return currentTarget;
-
         let target: ReadCall = null;
 
         for (let i = 0; i < calls.length; i++) {
@@ -33,11 +29,15 @@ export default class EDF extends DeltaToTargetAlgorithm implements AccessAlgorit
             }
         }
 
+        if (currentTarget != null && target != null && !this.SWITCH_TARGET_MID_FLY)
+            return currentTarget;
+
         /*
          * if there is no real time call, pick the closest one (SSTF)
+         * it should default to SSTF's switch_target_mid_fly
          */
         if (target == null) {
-            target = new SSTF().getNextTarget(current_position, calls);
+            target = new SSTF().getNextTarget(current_position, calls, max_position, current_time, currentTarget);
         }
 
         return target;
